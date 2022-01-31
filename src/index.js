7d4dea8d542db81af1b5273f467fb836d63c8b65
Haris Mashhood <71483410+HarisMashhood@users.noreply.github.com>
2022-01-31 19:44:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './StateProvider';
 import reducer, {initialState} from "./reducer";
